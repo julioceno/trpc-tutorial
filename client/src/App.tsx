4aeb556b8a6149fb2c1487b069376f0b0a43f358
julioceno <julioreplay@gmail.com>
2022-12-24 01:13:23 -0300

    [feature]: criando rota de createProduct

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { trpc } from './trpc';
 import { useState } from 'react';
 import { httpBatchLink } from '@trpc/client';
@@ -25,9 +25,36 @@ function App() {
 }
 
 function AppContent() {
-  /* const helloMessage = trpc.hello.useQuery(); */
+  const [newProduct, setNewProduct] = useState('');
+  const getProducts = trpc.getProducts.useQuery();
+  const addProduct = trpc.createProduct.useMutation();
 
-  return <div>Hello world</div>;
+  const client = trpc.useContext();
+
+  // Entender mais sobre o react query dps
+  if (getProducts.isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(newProduct);
+          addProduct.mutate(newProduct, {
+            onSuccess(value) {
+              client.getProducts.refetch();
+            },
+          });
+        }}
+      >
+        <input type='text' onChange={(e) => setNewProduct(e.target.value)} />
+        <button>Save</button>
+      </form>
+      <div>{JSON.stringify(getProducts.data)}</div>
+    </div>
+  );
 }
 
 export default App;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,43 @@
 import { initTRPC } from '@trpc/server';
+import { z } from 'zod';
 
 const t = initTRPC.create();
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
 
+const response = [
+  {
+    id: 1,
+    name: 'Product 1',
+    description: 'Description 1',
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    description: 'Description 2',
+  },
+];
+
 export const appRouter = router({
   hello: publicProcedure.query((req) => {
-    const {} = req;
-
     return 'hello 2';
+  }),
+
+  getProducts: publicProcedure.query((req) => {
+    return response;
+  }),
+
+  createProduct: publicProcedure.input(z.string()).mutation(({ input }) => {
+    const id = response.length + 1;
+    const newProduct = {
+      id,
+      name: input,
+      description: '',
+    };
+
+    response.push(newProduct);
+    return 'product created';
   }),
 });
 
